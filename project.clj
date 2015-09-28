d980a2ab522c13a16aa00d3a06d1dc66f1bf9ef2
Mischov <mmischov@gmail.com>
2015-09-28 10:48:15 -0500

    Updated to jsoup 1.8.3.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mischov/reaver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.jsoup/jsoup "1.8.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.jsoup/jsoup "1.8.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [criterium "0.4.3"]]
                    :warn-on-reflection true}})
